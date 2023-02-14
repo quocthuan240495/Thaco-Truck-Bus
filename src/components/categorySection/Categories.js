79ca62d3f64375b8004286d654942b2532dd8546
ntrongtin.olive <ntrongtin.olive@gmail.com>
2023-02-14 17:28:47 +0700

    slider slick

--- src/components/categorySection/Categories.js
+++ src/components/categorySection/Categories.js
@@ -1,11 +1,26 @@
 import React from "react"
 import { Container, Row, Col } from "reactstrap"
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import "./Categories.css"
 export default function Categories() {
+
+  const settings = {
+    // dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
+    cssEase: "linear"
+  };
+
   return (
     <div className="banner-section">
-      <Container fluid>
+      {/* <Container fluid>
         <Row>
           <Col lg="4">
             <div className="single-banner">
@@ -32,7 +47,59 @@ export default function Categories() {
             </div>
           </Col>
         </Row>
-      </Container>
+      </Container> */}
+      <div className="mainContainer">
+        <Slider {...settings}>
+          <div className="container">
+            <div className="single-banner">
+              <img src={require("../../images/banner-3.jpg")} alt="" />
+              <div className="inner-text">
+                <h4>Kid’s</h4>
+              </div>
+            </div>
+          </div>
+          <div className="container">
+            <div className="single-banner">
+              <img src={require("../../images/banner-3.jpg")} alt="" />
+              <div className="inner-text">
+                <h4>Kid’s</h4>
+              </div>
+            </div>
+          </div>
+          <div className="container">
+            <div className="single-banner">
+              <img src={require("../../images/banner-3.jpg")} alt="" />
+              <div className="inner-text">
+                <h4>Kid’s</h4>
+              </div>
+            </div>
+          </div>
+          <div className="container">
+            <div className="single-banner">
+              <img src={require("../../images/banner-3.jpg")} alt="" />
+              <div className="inner-text">
+                <h4>Kid’s</h4>
+              </div>
+            </div>
+          </div>
+          <div className="container">
+            <div className="single-banner">
+              <img src={require("../../images/banner-3.jpg")} alt="" />
+              <div className="inner-text">
+                <h4>Kid’s</h4>
+              </div>
+            </div>
+          </div>
+          <div className="container">
+            <div className="single-banner">
+              <img src={require("../../images/banner-3.jpg")} alt="" />
+              <div className="inner-text">
+                <h4>Kid’s</h4>
+              </div>
+            </div>
+          </div>
+        </Slider>
+      </div>
     </div>
   )
 }
